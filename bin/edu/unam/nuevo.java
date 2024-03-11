717061a9db5640743cbee5927f9ef8bc54e34083
CarloEmi <carlosspereyra.cp@gmail.com>
2024-03-11 08:17:33 -0300

    borrando carpeta de tutorial prueba

--- bin/edu/unam/nuevo.java
+++ bin/edu/unam/nuevo.java
@@ -2,6 +2,6 @@ package ControlExpedientes.bin.edu.unam;
 
 public class nuevo {
     public static void main(String[] args) {
-        System.out.println("Auaaaaaaaaaaaante AIRBAAGGGGGGGGGGGGG\n");
+        System.out.println("Hola mundo\n");
     }
 }
