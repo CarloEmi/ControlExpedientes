77983d15c485a596c744f2c563f949e8a245cab2
CarloEmi <carlosspereyra.cp@gmail.com>
2024-03-11 08:19:16 -0300

    se sube proyecto

--- bin/edu/unam/nuevo.java
+++
@@ -1,7 +0,0 @@
-package ControlExpedientes.bin.edu.unam;
-
-public class nuevo {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo\n");
-    }
-}
--- src/edu/unam/Tutorial.java
+++ src/edu/unam/Tutorial.java
@@ -4,6 +4,6 @@ public class Tutorial {
 
     public static void main(String[] args) {
 
-        System.out.println("Auaaaaaaaaaaaante AIRBAAGGGGGGGGGGGGG\n");
+        System.out.println("nuevo\n");
     }
 }
